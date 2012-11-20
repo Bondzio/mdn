85178f448b5dc713dbacd5d1bbe6967008893817
Steve Cervera <cervera1@mac.com>
2012-11-20 00:19:58 -0500

    updated homepage
    Former-commit-id: f35a759d331270727e293d0557e1183f90667b8f

--- app/cells/prayer_cell.rb
+++ app/cells/prayer_cell.rb
@@ -1,7 +1,7 @@
 class PrayerCell < Cell::Rails
 #SMC - This is the "Prayer stream controller used to insert a cell into the Home#Index page
 
-  def prayerstream
+  def prayerstream(prayer)
    #@prayers = Prayer.where(:moderated => true)
    #modified the record return so that the array is sorted randomly
    #this ensures that when the home page is refreshed, the order the prayers are displayed in changes
