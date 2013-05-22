ba16f1bafb103c40660e1888269bb447adc46810
Steve Cervera <cervera1@mac.com>
2013-05-22 16:44:02 -0400

    added migration for adding username to users model
    Former-commit-id: f6a122d75c4f2d7b8671fe00bcb337eb937e0b1b

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130518123202) do
+ActiveRecord::Schema.define(:version => 20130522193758) do
 
   create_table "admins", :force => true do |t|
     t.string   "email",                                 :default => "", :null => false
@@ -282,12 +282,12 @@ ActiveRecord::Schema.define(:version => 20130518123202) do
   end
 
   create_table "users", :force => true do |t|
-    t.string   "email",                                 :default => "", :null => false
-    t.string   "encrypted_password",     :limit => 128, :default => "", :null => false
+    t.string   "email"
+    t.string   "encrypted_password",     :limit => 128
     t.string   "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
-    t.integer  "sign_in_count",                         :default => 0
+    t.integer  "sign_in_count"
     t.datetime "current_sign_in_at"
     t.datetime "last_sign_in_at"
     t.string   "current_sign_in_ip"
@@ -301,17 +301,13 @@ ActiveRecord::Schema.define(:version => 20130518123202) do
     t.string   "zipcode"
     t.string   "provider"
     t.string   "uid"
-    t.string   "username"
     t.string   "role"
     t.boolean  "admin"
     t.integer  "roles_mask"
     t.string   "confirmation_token"
     t.datetime "confirmed_at"
     t.datetime "confirmation_sent_at"
+    t.string   "username"
   end
-
-  add_index "users", ["confirmation_token"], :name => "index_users_on_confirmation_token", :unique => true
-  add_index "users", ["email"], :name => "index_users_on_email", :unique => true
-  add_index "users", ["reset_password_token"], :name => "index_users_on_reset_password_token", :unique => true
 
 end
