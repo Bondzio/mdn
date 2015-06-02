082d009a83b478b0209bd3222193ee773739f583
Steve Cervera <cervera1@mac.com>
2015-06-02 11:31:36 -0400

    updated boot camp page

--- app/helpers/pages_helper.rb
+++ app/helpers/pages_helper.rb
@@ -1,5 +1,12 @@
 module PagesHelper
 
-
+	def time_to_display
+      launch_day = Time.local(2015, 6, 3, 12, 0)
+      if Time.now < launch_day
+      	return false
+      else
+      	return true
+      end  	
+	end
 
 end
